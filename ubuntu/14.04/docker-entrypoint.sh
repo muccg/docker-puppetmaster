0a2f491d8ee3319d99190cfa7db59d20863b5232
Adam Hunter <ahunter@ccg.murdoch.edu.au>
2015-04-11 00:16:41 +0800

    Initial import

---
+++ ubuntu/14.04/docker-entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function defaults {
+    : ${PUPPET_DNS_ALT_NAMES="localhost,puppetmaster,master"}
+
+    echo "PUPPET_DNS_ALT_NAMES is ${PUPPET_DNS_ALT_NAMES}"
+
+    export PUPPET_DNS_ALT_NAMES
+}
+
+defaults
+
+if [ "$1" = 'puppetmaster' ]; then
+    echo "[RUN]: Launching puppet master"
+    /usr/bin/puppet master --confdir "/data" --dns_alt_names "${PUPPET_DNS_ALT_NAMES}" --no-daemonize --verbose 2>&1 | tee /data/puppetmaster.log
+    exit $?
+fi
+
+echo "[RUN]: Builtin command not provided [puppetmaster]"
+echo "[RUN]: $@"
+
+exec "$@"
